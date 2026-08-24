718f80047c8ab8ae6b3640c7392166778a6cda70
Anam Hira <anamhira@Anams-Air.attlocal.net>
2026-08-24 01:06:50 -0700

    Pin AGP 8.7.2 so Expo prefab CMake can find x86_64 libraries.
    AGP 8.6 hits CXX1210 on expo-modules-core; 8.7 is the matcher fix that last made Android preview assemble.

--- plugins/withAndroidMinSdk.js
+++ plugins/withAndroidMinSdk.js
@@ -1,4 +1,8 @@
-const { createRunOncePlugin, withGradleProperties } = require('@expo/config-plugins');
+const {
+  createRunOncePlugin,
+  withGradleProperties,
+  withProjectBuildGradle,
+} = require('@expo/config-plugins');
 
 const pluginName = 'with-android-min-sdk';
 
@@ -12,16 +16,41 @@ function setGradleProperty(properties, key, value) {
   return properties;
 }
 
+function replaceNdkVersion(contents, ndkVersion) {
+  const next = contents.replace(/ndkVersion\s*=\s*.+/g, `ndkVersion = "${ndkVersion}"`);
+  if (next === contents) {
+    throw new Error(
+      `${pluginName}: android/build.gradle has no ndkVersion assignment to set ${ndkVersion}`
+    );
+  }
+  return next;
+}
+
+function replaceAgpClasspath(contents, agpVersion) {
+  const versioned = contents.replace(
+    /classpath\(['"]com\.android\.tools\.build:gradle:[^'"]+['"]\)/g,
+    `classpath('com.android.tools.build:gradle:${agpVersion}')`
+  );
+  if (versioned !== contents) {
+    return versioned;
+  }
+  return contents.replace(
+    /classpath\(['"]com\.android\.tools\.build:gradle['"]\)/g,
+    `classpath('com.android.tools.build:gradle:${agpVersion}')`
+  );
+}
+
 function withAndroidMinSdk(config, props) {
   const minSdkVersion = String((props && props.minSdkVersion) || 28);
   const architectures = String((props && props.architectures) || 'x86_64');
   const ndkVersion = String((props && props.ndkVersion) || '27.1.12297006');
+  const agpVersion = String((props && props.agpVersion) || '8.7.2');
   const newArchEnabled =
     props && Object.prototype.hasOwnProperty.call(props, 'newArchEnabled')
       ? String(props.newArchEnabled)
       : 'false';
 
-  return withGradleProperties(config, (modConfig) => {
+  config = withGradleProperties(config, (modConfig) => {
     modConfig.modResults = setGradleProperty(
       modConfig.modResults,
       'android.minSdkVersion',
@@ -41,6 +70,19 @@ function withAndroidMinSdk(config, props) {
       modConfig.modResults,
       'newArchEnabled',
       newArchEnabled
+    );
+    modConfig.modResults = setGradleProperty(
+      modConfig.modResults,
+      'android.native.buildOutput',
+      'verbose'
+    );
+    return modConfig;
+  });
+
+  return withProjectBuildGradle(config, (modConfig) => {
+    modConfig.modResults.contents = replaceAgpClasspath(
+      replaceNdkVersion(modConfig.modResults.contents, ndkVersion),
+      agpVersion
     );
     return modConfig;
   });
